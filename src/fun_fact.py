6b5ed226e4d5b237ab530705efb85fcd7fce116c
Khady71 <khady@MATEGAYE.localdomain>
2026-02-27 15:17:58 +0100

    restructuration

---
+++ src/fun_fact.py
@@ -0,0 +1,16 @@
+def get_michael_scott_quote():
+    quotes = [
+        "I'm not superstitious, but I am a little stitious.",
+        "Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.",
+        "I declare BANKRUPTCY!",
+        "I feel like my kids grew up, and then they married each other. It’s every parent’s dream.",
+        "I’m an early bird and I’m a night owl. So I’m wise and I have worms.",
+        "Sometimes I'll start a sentence and I don't even know where it's going. I just hope I find it along the way.",
+        "Toby is in HR, which technically means he works for corporate, so he's really not a part of our family.",
+        "The worst thing about prison was the Dementors.",
+        "Identify theft is not a joke, Jim! Millions of families suffer every year! (Wait, that was Dwight.)",
+        "You miss 100% of the shots you don’t take. — Wayne Gretzky — Michael Scott"
+    ]
+    
+    
+    return random.choice(quotes)
--- src/main.py
+++ src/main.py
@@ -3,27 +3,12 @@ import random
 import time
 import requests
 from user import User
+from fun_fact import get_michael_scott_quote
 
 
 
-def get_michael_scott_quote():
-    quotes = [
-        "I'm not superstitious, but I am a little stitious.",
-        "Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.",
-        "I declare BANKRUPTCY!",
-        "I feel like my kids grew up, and then they married each other. It’s every parent’s dream.",
-        "I’m an early bird and I’m a night owl. So I’m wise and I have worms.",
-        "Sometimes I'll start a sentence and I don't even know where it's going. I just hope I find it along the way.",
-        "Toby is in HR, which technically means he works for corporate, so he's really not a part of our family.",
-        "The worst thing about prison was the Dementors.",
-        "Identify theft is not a joke, Jim! Millions of families suffer every year! (Wait, that was Dwight.)",
-        "You miss 100% of the shots you don’t take. — Wayne Gretzky — Michael Scott"
-    ]
+
     
-    
-    return random.choice(quotes)
-    
-
 def create_user_from_server():
 
     print("\n Connecting to IBE server")
@@ -89,6 +74,7 @@ def menu_principal(user):
         elif choice == "5":
             print("Goodbye, catch you on the flippity flip!")
             break
+
 
 
 def main():
